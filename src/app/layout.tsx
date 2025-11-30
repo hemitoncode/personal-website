63b9e55c7f8d41c0e79e8495e43596f9e929228a
Hemit Patel <hemitvpatel@gmail.com>
2025-11-30 15:54:34 -0500

    remove components + update title of website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Hemit Patel | Personal Website",
+  title: "Hemit Patel",
   description: "Hemit Patel's Personal Website",
 };
 
